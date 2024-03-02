720f6d8e8e0d7f5f5c1fa694b3b838441b854459
Saurav <sauravkmr780@gmail.com>
2024-03-03 02:26:40 +0530

    cypress fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,19 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
+
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
