70dfe9f10e8341ac188b05ce100a1fc7a055fddb
Saurav <sauravkmr780@gmail.com>
2024-03-03 01:53:32 +0530

    cypress reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  "reporter": "cypress-multi-reporters",
+  "reporterOptions": {
+    "configFile": "reporter-config.json"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
