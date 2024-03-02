8eeefe391605ae899abc22a5edd7cdb1eeae4e61
Saurav <sauravkmr780@gmail.com>
2024-03-03 03:40:43 +0530

    cypress report

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,16 @@
-const { defineConfig } = require("cypress");
-
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
-    reporter: '../node_modules/cypress-mochawesome-reporter',
-    video: true,
-    reporterOptions: {
-      reportDir: 'reports/mochawesome',
-      charts: true,
-      overwrite: false,
-      html: false,
-      json: true,
-      reportFilename: "Orange Cypress Test Report",
-      reportPageTitle: 'Cypess-Report',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
-    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
